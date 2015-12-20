ba63dc6e3ecbc903158542ebfc4499de2f31bb53
apple <zhengchen@youzan.com>
2015-12-20 21:47:50 +0800

    getFrequentRenterPoints方法移动到Rental类中，测试通过

--- service/src/main/java/com/apple/model/Customer.java
+++ service/src/main/java/com/apple/model/Customer.java
@@ -44,10 +44,7 @@ public class Customer {
   }
 
   private int getFrequentRenterPoints(Rental each) {
-    // add bonus for a two day new release rental
-    if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-      return 2;
-    return 1;
+    return each.getFrequentRenterPoints();
   }
 
 }
