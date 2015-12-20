8577229b8385747fb3da57a3d559a47b58ed32cf
apple <zhengchen@youzan.com>
2015-12-20 21:26:24 +0800

    用查询方法内联掉临时变量，测试通过

--- service/src/main/java/com/apple/model/Customer.java
+++ service/src/main/java/com/apple/model/Customer.java
@@ -29,9 +29,7 @@ public class Customer {
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) { // 租借多个片子时
-      double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
-      thisAmount = each.getCharge();
 
       // add frequent renter points
       frequentRenterPoints ++;
@@ -40,8 +38,8 @@ public class Customer {
         frequentRenterPoints ++;
 
       // show figures for this rental
-      result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+      result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
     }
 
     // add footer lines
