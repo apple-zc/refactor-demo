1594ebe7b3b21e974b190967b15eea62a7229807
apple <zhengchen@youzan.com>
2015-12-20 19:55:13 +0800

    重构的初始化状态

---
+++ service/src/main/java/com/apple/model/Rental.java
@@ -0,0 +1,23 @@
+package com.apple.model;
+
+/**
+ * @author apple created at: 15/12/20 下午7:09
+ */
+public class Rental {
+  private Movie _movie;
+
+  private int _daysRented; // 租期
+
+  public Rental(Movie movie, int daysRented) {
+    _movie = movie;
+    _daysRented = daysRented;
+  }
+
+  public int getDaysRented() {
+    return _daysRented;
+  }
+
+  public Movie getMovie() {
+    return _movie;
+  }
+}
