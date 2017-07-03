62f755e2b8cf7c3bfeff516222dffcb796550af2
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-07-03 01:43:14 -0500

    extract getCharge and getFrequentRenterPoints method and move , replace thisAmount with query

--- service/src/main/java/com/apple/model/Customer.java
+++ service/src/main/java/com/apple/model/Customer.java
@@ -29,21 +29,11 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) { // 租借多个片子时
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            // determine amounts for each line
-            thisAmount = getThisAmount(each);
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
+            frequentRenterPoints += getFrequentRenterPoints(each);
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         // add footer lines
@@ -52,23 +42,12 @@ public class Customer {
         return result;
     }
 
-    private double getThisAmount(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+    private int getFrequentRenterPoints(Rental rental){
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
     }
+
+
 }
