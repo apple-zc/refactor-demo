408affec45e0c4f711e25a21becb39addcda8de5
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-07-03 01:53:02 -0500

    Replace totalAmount and frequentRenterPoints with query method, during this I made a mistake for returning a double value on getTotalF.. method. Thanks for the test, I found it.

--- service/src/main/java/com/apple/model/Customer.java
+++ service/src/main/java/com/apple/model/Customer.java
@@ -24,22 +24,39 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0; // 消费总额
-        int frequentRenterPoints = 0; // 常客积分
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) { // 租借多个片子时
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += getFrequentRenterPoints(each);
+
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int totalFrequentRenterPoints = 0; // 常客积分
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) { // 租借多个片子时
+            Rental each = (Rental) rentals.nextElement();
+            totalFrequentRenterPoints += getFrequentRenterPoints(each);
+        }
+        return totalFrequentRenterPoints;
+    }
+
+    private double getTotalAmount() {
+        double totalAmount = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            totalAmount += each.getCharge();
+        }
+        return totalAmount;
     }
 
     private int getFrequentRenterPoints(Rental rental){
@@ -48,6 +65,8 @@ public class Customer {
         else
             return 1;
     }
+
+
 
 
 }
